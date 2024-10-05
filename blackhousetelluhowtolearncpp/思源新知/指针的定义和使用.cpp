4f7491e779f047d407a80f058b66ed23308c1c45
HsxMark <hsxmark@outlook.com>
2024-10-06 00:02:38 +0800

    feat(vector): add new file of cpp vector

--- blackhousetelluhowtolearncpp/思源新知/指针的定义和使用.cpp
+++ blackhousetelluhowtolearncpp/思源新知/指针的定义和使用.cpp
@@ -1,15 +1,20 @@
+// 指针的定义和使用
 #include <iostream>
 using namespace std;
 int main()
 {
   // 1.定义指针
   int a = 10;
-  // 语法：数据类型*指针变量名;
+  // 指针定义的语法：数据类型 * 指针变量
   int *p;
+  // 让指针记录变量地址
   p = &a;
-  cout << "a的地址为：" << &a << endl;
+  cout << "a的地址为多少：" << &a << endl;
   cout << "指针p为：" << p << endl;
-  // 2.使用指针
 
+  // 2.使用指针
+  // 指针前加*代表解引用找到指针指向的内存的数据
+  cout << "a= " << a << endl;
+  cout << "*p=" << *p << endl;
   return 0;
 }
---
+++ blackhousetelluhowtolearncpp/思源新知/野指针和空指针.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int *p=NULL;
+  //*p=100;//会报错
+  //空指针用于变量初始化，并且无法访问，0~255内存编号是系统占用的，不可访问
+  return 0;
+}

8ccda8e059ddd9d0f1a785395745e732d740ad28
HsxMark <hsxmark@outlook.com>
2024-10-14 02:37:50 +0800

    fix(vector): fix new notes

--- blackhousetelluhowtolearncpp/思源新知/指针和函数.cpp
+++ blackhousetelluhowtolearncpp/思源新知/指针和函数.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 using namespace std;
-//实现两个数字交换
-void swap01(int a,int b)
-  {
-    int temp=a;
-    a=b;
-    b=temp;
-  }
+// 实现两个数字交换
+void swap01(int a, int b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+}
+void swap02(int *p1, int *p2)
+{
+  int temp = *p1;
+  *p2 = *p1;
+  *p1 = temp;
+}
 int main()
 {
-  //指针与函数
-  //1.值传递
-  int a=10;
-  int b=20;
-  swap01(a,b);
-  //2.地址传递
+  // 指针与函数
+  // 1.值传递
+  int a = 10;
+  int b = 20;
+  swap01(a, b);
+  // 2.地址传递
+  swap02(&a, &b);
+  cout << "a=" << a << endl;
+  cout << "b=" << b << endl;
 }
